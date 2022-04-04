af15632a395547afc90b69809a434a5204d391bb
trungthien2001 <82750730+trungthien2001@users.noreply.github.com>
2022-04-03 20:37:55 -0600

    Update auth_jwt.js

--- auth_jwt.js
+++ auth_jwt.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var User = require('./Users');
-
+require("dotenv").config()
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
 opts.secretOrKey = process.env.SECRET_KEY;
